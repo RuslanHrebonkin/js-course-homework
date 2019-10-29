9784e033546a633b11de956ccc0aa22344d26a34
RuslanHrebonkin <51698667+RuslanHrebonkin@users.noreply.github.com>
2019-10-29 23:32:58 +0200

    Create homeWork4part2.js

---
+++ homeWork4part2.js
@@ -0,0 +1,5 @@
+ // import class Controller from 'C:\\Users\\Rus\\PhpstormProjects\\untitled\\homeWork4.js';
+const class Controller = require('C:\\Users\\Rus\\PhpstormProjects\\untitled\\homeWork4.js');
+let a = new Controller();
+a.changeFinish;
+console.log(a.finished);
